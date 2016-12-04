2c5dd198fffcc6179d3cfb5d27de17b93d369e29
Sheena Nickerson <sheenanick@gmail.com>
2016-12-03 18:32:44 -0800

    make api call functional and display duration

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,8 +1,8 @@
 var Alarm = require('./../js/alarm.js').alarmModule;
 var apiKey = require('./../.env').apiKey;
 
-getDuration(start, destination) {
-  $.get('https://maps.googleapis.com/maps/api/directions/json?mode=transit&origin=' + start + '&destination=' + destination + "&key=" + apiKey).then(function(response) {
+function getDuration(origin, destination) {
+  $.get('https://maps.googleapis.com/maps/api/directions/json?mode=transit&origin=' + origin + '&destination=' + destination + '&key=' + apiKey).then(function(response) {
     $('.duration').text(response.routes[0].legs[0].duration.text);
   }).fail(function(error) {
     $('.duration').text(error.responseJSON.message);
@@ -10,41 +10,41 @@ getDuration(start, destination) {
 }
 
 $(function() {
-  getDuration("Portland,OR", "Seattle,WA");
-  var now;
-  var audio = new Audio('http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2015.mp3');
-  setInterval(function(){
-    now = moment();
-    $('#time').text(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-  }, 1000);
-
-  var soundAlarm = function(time) {
-    setTimeout(function() {
-      $(".sound-alarm").show();
-      $("body").addClass("alarm");
-      audio.play();
-    }, time);
-  }
-
-  var stopAlarm = function() {
-    $(".sound-alarm").hide();
-    $("body").removeClass("alarm");
-    audio.pause();
-  }
-
   $("form").submit(function(event) {
     event.preventDefault();
-    var userAlarm = moment($("#alarm").val());
-    var alarm = new Alarm(userAlarm);
-    soundAlarm(alarm.alert(now));
+    var destination = $("#destination").val();
+    getDuration('Portland,OR', destination);
+    $('.duration-div').show();
+    $('#stop-button').show();
    });
 
-  $("#snooze").click(function(event) {
-    stopAlarm();
-    soundAlarm(100000);
-  });
-
-  $("#stopAlarm").click(function(event) {
-    stopAlarm();
-  });
+  // var now;
+  // var audio = new Audio('http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2015.mp3');
+  // setInterval(function(){
+  //   now = moment();
+  //   $('#time').text(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  // }, 1000);
+  //
+  // var soundAlarm = function(time) {
+  //   setTimeout(function() {
+  //     $(".sound-alarm").show();
+  //     $("body").addClass("alarm");
+  //     audio.play();
+  //   }, time);
+  // }
+  //
+  // var stopAlarm = function() {
+  //   $(".sound-alarm").hide();
+  //   $("body").removeClass("alarm");
+  //   audio.pause();
+  // }
+  //
+  // $("#snooze").click(function(event) {
+  //   stopAlarm();
+  //   soundAlarm(100000);
+  // });
+  //
+  // $("#stopAlarm").click(function(event) {
+  //   stopAlarm();
+  // });
 });
--- js/alarm.js
+++ js/alarm.js
@@ -1,15 +1,5 @@
-var apiKey = require('./../.env').apiKey;
-
 function Alarm(alarm) {
   this.alarm = alarm;
-}
-
-Alarm.prototype.getDuration(start, destination) {
-  $.get('https://maps.googleapis.com/maps/api/directions/json?mode=transit&origin=' + start + '&destination=' + destination + "&key=" + apiKey).then(function(response) {
-    $('.duration').text("The duration is " + response.routes[0].legs[0].duration.text);
-  }).fail(function(error) {
-    $('.duration').text(error.responseJSON.message);
-  });
 }
 
 Alarm.prototype.alert = function(now) {
